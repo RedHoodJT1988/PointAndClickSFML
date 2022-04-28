1c572b57d95910069d07871fb756dd47bc4dc817
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-04-27 21:24:09 -0700

    Add project files.

---
+++ PointAndClickSFML/enemy.h
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+#ifndef ENEMY_H
+#define ENEMY_H
+
+class Enemy
+{
+	sf::Texture enemyTexture;
+	sf::Sprite enemySprite;
+	sf::SoundBuffer attacSoundBuffer;
+	sf::Sound attackSound;
+
+public:
+	int energy;
+	Enemy(int);
+	bool performSetup();
+	bool checkIfHit(sf::Vector2i);
+	bool takeDamage(int);
+	void draw(sf::RenderWindow*);
+};
+
+#endif
